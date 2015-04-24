52872b1784f883167a3005b3b1b305f044a2c9ad
rhoen+Alex <rhoen+Alex@appacademy.io>
2015-04-24 16:31:25 -0400

    Add toggleTurn function to Game class

--- ttt/game.js
+++ ttt/game.js
@@ -1,18 +1,28 @@
 var Board = require('ttt/board.js')
-var readerThing  () {
-  function () {
-    var readline = require('readline');
-    var reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-});
 
 function Game (readerThing) {
+  this.readline = require('readline');
+  this.reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  };
+  this.turn = "player1";
 
   //game stuff
 };
 
+Game.prototype.toggleTurn = function  () {
+  if (this.turn === "player1") {
+    this.turn = "player2";
+  } else {
+    this.turn = "player1";
+  }
+}
+
 Game.prototype.input = function () {
   reader.question(ljksfdkljfdsjlksfdjlkfds)
+}
+
+Game.prototype.run = function (completionCallback) {
+
 }
